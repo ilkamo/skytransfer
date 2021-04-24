4a464baebc17de7d566d4174af1fc946492059bc
Kamil Molendys <kamilmolendys@gmail.com>
2021-04-24 12:43:14 +0200

    Updated README.md and about section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -10,7 +10,7 @@ const About = () => {
       </p>
       <p>
         <span className="title">SkyTransfer</span> is an open source
-        decentralized file sharing platform. Do you have difficult times{' '}
+        decentralized file sharing platform. Do you have a difficult time{' '}
         <strong>sharing files between devices</strong>? Are you writing an email
         and you need to <strong>attach different files</strong>? Are you
         struggling with a ton of <strong>pictures to share</strong>? Let's try
@@ -22,11 +22,11 @@ const About = () => {
         <strong>single link</strong> or <strong>QR code</strong>.
       </p>
       <p>
-        Uploaded files are <strong>encrypted using AES algorithm</strong> so no
-        one can access them without your permission. Only{' '}
-        <strong>sharing the link</strong>, other people can{' '}
+        Uploaded files are <strong>encrypted using the AES algorithm</strong> so
+        no one can access them without your permission. Only{' '}
+        <strong>by sharing the link</strong>, other people can{' '}
         <strong>download and decrypt</strong> the content you uploaded. In
-        addition, <strong>sharing a draft</strong>, people can continue
+        addition, <strong>by sharing a draft</strong>, people can continue
         uploading into the same SkyTransfer{' '}
         <strong>adding more files to it</strong>. Be careful when you share a
         draft!
@@ -37,14 +37,18 @@ const About = () => {
       </p>
       <p>
         Last but not least, you can <strong>Public your SkyTransfer</strong>.
-        What does it exactly mean? Let's explain in with an example. Imagine{' '}
+        What does it exactly mean? Let's explain it with an example. Imagine{' '}
         SkyTransfer like a box where you can put inside different files. This
         box can be opened by others only if they have the correct key. There is
         no other way of doing it. By publishing a SkyTransfer, you send this box
-        with all the files inside on the public Skynet. In addition, you are
-        attaching the keys to it so, when others find it, they can use the keys
-        to open it. Publish a SkyTransfer only when you are really aware of the
+        with all files on the public Skynet. In addition, you are attaching the
+        keys to it so, when others find it, they can use the keys to open it.
+        Publish a SkyTransfer only when you are really aware of the
         consequences!
+      </p>
+      <p>
+        SkyTransfer is still in development. Please report any issue, new idea
+        or bug.
       </p>
     </div>
   );
