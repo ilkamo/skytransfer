bac3b573e08a47d69a32b109769ebf5cbaeaff22
Michal Sokolowski <0michalsokolowski0@gmail.com>
2021-04-25 11:49:32 +0200

    Fix

--- src/crypto/crypto.ts
+++ src/crypto/crypto.ts
@@ -11,6 +11,7 @@ export interface FileDecrypt {
   decrypt(): Promise<File>;
 }
 
+
 export const deriveEncryptionKeyFromKey = (key): string => {
   return genKeyPairFromSeed(`${key}-aes-encrypt`).privateKey;
 };
