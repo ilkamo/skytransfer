d7ae325ca581e51c1359d526b117853a9efa616d
Kamil Molendys <kamilmolendys@gmail.com>
2021-11-03 20:37:34 +0100

    public path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.tsx',
   output: {
-    publicPath: "/",
+    publicPath: "build/",
     path: path.join(__dirname, 'build'),
     filename: 'index.bundle.js',
   },
