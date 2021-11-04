8201fba6f5f30eae87c6bc9488962ab595f30131
Kamil Molendys <kamilmolendys@gmail.com>
2021-11-04 20:41:07 +0100

    v2.0.2

--- src/components/uploader/dragger-content.tsx
+++ src/components/uploader/dragger-content.tsx
@@ -14,7 +14,7 @@ export const DraggerContent = ({
       <p className="ant-upload-drag-icon">
         <SkyTransferLogo />
         <br />
-        <span className="ant-upload-drag-icon version">SkyTransfer v2.0.1</span>
+        <span className="ant-upload-drag-icon version">SkyTransfer v2.0.2</span>
       </p>
       <p className="ant-upload-text">
         {onlyClickable ? (
